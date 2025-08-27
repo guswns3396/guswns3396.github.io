ba77741a06e4765cfa9d02eb0a287925a5abf0f2
guswns3396 <yhyunjoon@gmail.com>
2025-08-27 00:15:27 +0000

    Deploying to gh-pages from @ guswns3396/guswns3396.github.io@91ab74241ac82cc596310457dcb6db100ae015dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,6 +479,13 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0003-4424-2514", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=l1SIXHUAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
